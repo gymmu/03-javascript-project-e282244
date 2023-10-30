794e8e8802f474c65acf90cbecc95b2e55eb680a
Lars Burkard <e282244@edu.sbl.ch>
2023-10-30 12:11:03 +0100

    Aufgabe 1 und 2 (wurde von falschem Projekt von MIR eingefügt)

--- script.js
+++ script.js
@@ -4,7 +4,41 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+
+    //Wenn das aktuelle Zeichen 'e' ist, hängt an  die Liste an
+    if (currentElement === 'e') {
+      result.push("")
+      //Wenn das aktuelle Zeichen ein 'E' ist, hänge nichts an die Liste an
+    } else if (currentElement === 'E') {
+      result.push("")
+    } else { //Das aktuelle zeichen ist weder ein'e' noch ein 'E', hängt also das aktuelle Zeichen an
+      result.push(currentElement)
+    }
+  
   }
   return result.join("")
+
+}
+
+
+
+
+
+
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    //Wandle das aktulle Element in einen Grossbuchstaben um
+    const capitalizedElement = currentElement.toUpperCase()
+
+    //Hänge den Grossbuchstaben an das Resultat an...
+    result.push(capitalizedElement)
+}
+return result.join("")
+
 }
