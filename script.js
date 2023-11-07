8ae35885663f05e9675ee1ed3cde878ba56e18b7
Lars Burkard <e282244@edu.sbl.ch>
2023-11-07 09:23:58 +0100

    Aufgabe 5

--- script.js
+++ script.js
@@ -81,12 +81,73 @@ export function aufgabe04(args) {
 
 }
 
+export function aufgabe05(args) {
+  const input = args
+  const result = []
+  let capitalLetters = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    //sollte ein Grossbuchstabe erkennen¨
+    if (currentElement===".") {}
+    else if (currentElement===" ") {}
+    //ist ein element gleich, so rechnet es + 1.
+    else if(currentElement === currentElement.toUpperCase() )
+    {capitalLetters++}
+
+   }
+   if (capitalLetters>0) {
+    return true
+    
+   }
+   else {
+    return false
+   }
+}
+
+
+
+
+
+export function aufgabe06(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe07(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+
+export function aufgabe09(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+  
+
+  }
+}
+
+
+
+
+
 
 export function aufgabe08(args) {
   const input = args
   const result = []
-  let count = 1
-
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
 
@@ -108,18 +169,192 @@ export function aufgabe08(args) {
 }
 
 
+export function aufgabe10(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
 
+  }
+  return result.join("")
+}
 
+export function aufgabe11(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
 
+  }
+  return result.join("")
+}
 
+export function aufgabe12(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
 
-  
+  }
+  return result.join("")
+}
 
+export function aufgabe13(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
 
+  }
+  return result.join("")
+}
 
-  
-  
-  
+export function aufgabe14(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
 
-    
-      
+  }
+}
+
+export function aufgabe15(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe16(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe17(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe18(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe19(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe20(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe21(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe22(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe23(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe24(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe25(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe26(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe27(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
+export function aufgabe28(args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+  }
+  return result.join("")
+}
+
